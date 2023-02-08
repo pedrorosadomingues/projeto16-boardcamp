ae5c945e689af3e547634e10bcbb4d0784c32547
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-02-07 23:37:04 -0300

    feat: getGames() Routes

---
+++ src/Routes/index.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import  gamesRouter  from "./games.routes.js";
+
+const router = Router();
+
+router.use([gamesRouter]);
+
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import routes from "./Routes/index.routes.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
+
 
 
 const PORT = process.env.PORT || 5000;
