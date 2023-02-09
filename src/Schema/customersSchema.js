46a28787a7dab2905db13457fe99a8ad6c6e0a61
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-02-08 23:14:52 -0300

    fix/customerSchema cpf

--- src/Schema/customersSchema.js
+++ src/Schema/customersSchema.js
@@ -1,7 +1,7 @@
 import joi from  'joi' ;
 
 export const customerSchema = joi.object({
-    cpf: joi.string().min(11).max(11).required(),
+    cpf: joi.string().length(11).regex(/^\d+$/).required(),
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
     birthday: joi.date().required(),
