9a3d25cd6e9235e54a0b50725d2091ea5aef6d52
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-02-09 23:00:03 -0300

    fix/deleteRental

--- src/middleware/validateDeleteRental.js
+++ src/middleware/validateDeleteRental.js
@@ -1,23 +1,20 @@
 import { db } from "../database/database.connection.js";
 
 export async function validateDeleteRental(req, res, next) {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  try {
-    const rental = await db.query(
-      "SELECT * FROM rentals WHERE id = $1",
-      [id]
-    );
-    if (!rental.rows.length) {
-      return res.sendStatus(404);
+    try {
+        const rental = await db.query("SELECT * FROM rentals WHERE id = $1",  [id] );
+        if (!rental.rows.length) {
+            return res.sendStatus(404);
+        }
+        if (!rental.rows[0].returnDate) {
+            return res.sendStatus(400);
+        }
+        res.locals.rental = rental.rows[0];
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
     }
-    if (rental.rows[0].returnDate) {
-      return res.sendStatus(400);
-    }
-    res.locals.rental = rental.rows[0];
-    next();
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
 }
