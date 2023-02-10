bc2f441abb8872d688aeb5b660d73895d5365676
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-02-09 21:14:24 -0300

    fix/POST/rentals regras de negocio

--- src/Routes/rentals.routes.js
+++ src/Routes/rentals.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getRentals, insertRental, deleteRental} from "../controllers/rentals.controllers.js";
 import { validateRental } from "../middleware/validateRental.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import { rentalSchema } from "../Schema/rentalSchema.js";
 
 const router = Router();
 
 router.get("/rentals", getRentals); 
-router.post("/rentals", validateRental, insertRental);
+router.post("/rentals", validateSchema(rentalSchema), validateRental, insertRental);
 router.delete("/rentals/:id", deleteRental);
 
 export default router;
